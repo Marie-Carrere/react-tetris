4cb5ef38a21184df6a33f0b085da068628dd9789
marie.carrere <marie.carrere@zenika.com>
2019-12-31 15:23:34 +0100

    :sparkles: Add components, helper function and tetromino func

---
+++ src/components/Tetris.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+import { createStage } from '../gameHelpers';
+
+const Tetris = () => {
+  
+  return (
+    <div>
+      <Stage stage={createStage()} />
+      <aside>
+        <Display text='Score' />
+        <Display text='Rows' />
+        <Display text='Level' />
+      </aside>
+      <StartButton />
+    </div>
+  );
+};
+
+export default Tetris;
